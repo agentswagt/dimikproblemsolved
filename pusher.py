987a561bb4dfb65d0528bddef8836f84d5c132b3
Agentswagt <shagatoc@gmail.com>
2021-04-02 08:25:38 +0600

    commiter

--- pusher.py
+++ pusher.py
@@ -3,6 +3,8 @@ import time
 i = 0
 while i < 100000:
     os.system("python pushcommand.py")
-    time.sleep(20)
-    i += 1
+    
+    
     print("Pushing for {} times".format(i))
+    time.sleep(10)
+    i += 1
